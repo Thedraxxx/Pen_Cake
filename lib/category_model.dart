734dbdece0a6da8fbd5ef71094c970013db281a0
Roshan Gurung <roshc10grg@gmail.com>
2024-06-29 12:49:16 +0545

    updates

---
+++ lib/category_model.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class CategoryModel {
+  String name;
+  Color boxcolor;
+
+   CategoryModel({required this.name, required this.boxcolor});
+   static List<CategoryModel> getCategories() {
+    List<CategoryModel> categories = [];
+
+    categories.add(CategoryModel(name: 'Salad', boxcolor: Color(0xff92A3FD)));
+    categories.add(CategoryModel(name: 'Cake', boxcolor: Color(0xffC58BF2)));
+    categories.add(CategoryModel(name: 'Burger', boxcolor: Color(0xff92A3FD)));
+    categories.add(CategoryModel(name: 'Pizza', boxcolor: Color(0xffC58BF2)));
+
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,90 @@
+import 'package:clear/category_model.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const FoodApp());
 }
-
 class FoodApp extends StatelessWidget {
   const FoodApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Food App',
       home: HomePage(),
     );
   }
 }
-
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+ 
 
+ 
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
+   List<CategoryModel> categories =[];
+ void _getCategories()
+  {
+   categories = CategoryModel.getCategories();
+  }
   @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blueGrey.shade100,
       appBar: appBar(),
       body: Column(
-        children: [
+        children: <Widget>[
           Container(
             margin: EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: TextField(
-              decoration:
-               InputDecoration(fillColor: Colors.blue,
-                  prefixIcon: Icon(Icons.search),
-                  filled: true,
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide.none),
-                  hintText: "Search panecake",
-                  suffixIcon: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Icon(Icons.mic),
-                    ],
-                  )),
-            ),
-          )
-        ],
+            child:TextField(
+  decoration: InputDecoration(
+    fillColor: Colors.white,
+    prefixIcon: Icon(Icons.search, size: 25),
+    filled: true,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: BorderSide.none,
+    ),
+    hintText: "Search pancake",
+    hintStyle: TextStyle(color: Colors.black),
+    suffixIcon: Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Icon(Icons.mic),
+    ),
+  ),
+),
+ ),
+ SizedBox(height: 20,),
+ Row(mainAxisAlignment: MainAxisAlignment.start,
+   children: [
+     Padding(
+       padding: const EdgeInsets.only(left: 20),
+       child: Text("Category",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+     ),
+   ],
+ ),
+ SizedBox(height: 10,),
+   Container(
+    height: 150,
+    child: ListView.builder(
+      itemCount: categories.length,
+      itemBuilder: (context,index){
+      return Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: categories[index].boxcolor,
+        ),
+      );
+    },
+   ),
+       ) ],
       ),
     );
   }
-
   AppBar appBar() {
     return AppBar(
       leading: Padding(
@@ -83,7 +113,7 @@ class _HomePageState extends State<HomePage> {
             color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
       ),
       centerTitle: true,
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blueGrey.shade100,
       elevation: 0.0,
     );
   }
